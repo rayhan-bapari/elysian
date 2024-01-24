3e36baffe2a791f6e2a01736ddada1ea7d0ebd7f
developerrayhan <developerrayhan15@gmail.com>
2024-01-24 22:45:40 +0600

    Create header and working with hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "*.html"],
     theme: {
+        colors: {
+            primary: "#C1DEE8",
+            secondary: "#FBD9B9",
+            black: "#000000",
+            gray: "#1F1F1F",
+            gray2: "#fafafa",
+            offwhite: "#BEBEBE",
+            white: "#FFFFFF",
+            white2: "#fefefe",
+        },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1200px",
+            "2xl": "1536px",
+            "3xl": "1920px",
+        },
         extend: {},
     },
     plugins: [],
